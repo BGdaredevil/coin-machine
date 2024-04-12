bbf07ead2b5c8a4619cc6f3324115ff7cf454ff8
dimitar.dimitrov <dimitar.dimitrov@sirma.bg>
2024-04-12 19:26:38 +0300

    add toast service

---
+++ client/src/utils/toast.ts
@@ -0,0 +1,18 @@
+import { ReactNode } from "react";
+import { ToastOptions, toast } from "react-toastify";
+
+export function toastError(message: string | ReactNode, options?: ToastOptions) {
+    return toast.error(message, { position: "top-right", autoClose: 5000, ...options } as ToastOptions);
+}
+
+export function toastSuccess(message: string | ReactNode, options?: ToastOptions) {
+    return toast.success(message, { position: "top-right", autoClose: 5000, ...options } as ToastOptions);
+}
+
+export function toastWarning(message: string | ReactNode, options?: ToastOptions) {
+    return toast.warning(message, { position: "top-right", autoClose: 5000, ...options } as ToastOptions);
+}
+
+export function toastInfo(message: string | ReactNode, options?: ToastOptions) {
+    return toast.info(message, { position: "top-right", autoClose: 5000, ...options } as ToastOptions);
+}
