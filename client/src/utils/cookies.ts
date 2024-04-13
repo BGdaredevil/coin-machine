aacaee4dc2f0e83abc69073b00bcf17859346fe9
dimitar.dimitrov <dimitar.dimitrov@sirma.bg>
2024-04-13 19:18:31 +0300

    expose cookies set options

--- client/src/utils/cookies.ts
+++ client/src/utils/cookies.ts
@@ -1,6 +1,6 @@
 import Cookies from "js-cookie";
 
-const set = (name: string, value: string) => Cookies.set(name, value);
+const set = (name: string, value: string, options: Cookies.CookieAttributes = {}) => Cookies.set(name, value, options);
 
 const get = (name: string) => Cookies.get(name);
 
@@ -9,5 +9,5 @@ const remove = (name: string) => Cookies.remove(name);
 export default {
     set,
     get,
-    remove
+    remove,
 };
