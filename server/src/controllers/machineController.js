9935e70c6fdc95909a3f3b7a059c18813c95849f
dimitar.dimitrov <dimitar.dimitrov@sirma.bg>
2024-04-15 14:07:30 +0300

    add purchase capability

--- server/src/controllers/machineController.js
+++ server/src/controllers/machineController.js
@@ -3,6 +3,18 @@ import AuthMiddleware from "../middlewares/authMiddleware.js";
 import MachineService from "../services/machineService.js";
 import ProductService from "../services/productService.js";
 
+const coinKeys = ["twoDCoin", "oneDCoin", "fiftyCCoin", "twentyCCoin", "tenCCoin", "fiveCCoin", "twoCCoin", "oneCCoin"];
+const coinValueMap = {
+    oneCCoin: 0.01,
+    twoCCoin: 0.02,
+    fiveCCoin: 0.05,
+    tenCCoin: 0.1,
+    twentyCCoin: 0.2,
+    fiftyCCoin: 0.5,
+    oneDCoin: 1,
+    twoDCoin: 2,
+};
+
 const router = Router();
 
 const publicLoadAllByOwner = async (req, res, next) => {
@@ -122,8 +134,160 @@ const create = async (req, res) => {
     }
 };
 
-const inventoryEdit = async (req, res) => {
-    console.log(req.body);
+// const inventoryEdit = async (req, res) => {
+//     console.log(req.body);
+// };
+const purchase = async (req, res) => {
+    // console.log(req.body);
+    try {
+        const machineId = req.params.machineId;
+        const productId = req.params.productId;
+        const qty = req.body.qty;
+        const userAddedCoins = req.body.userAddedCoins;
+        const machine = await MachineService.getOneRaw(machineId);
+
+        const productRef = machine.inventory.find((e) => e.item._id.toString() === productId);
+
+        if (!productRef) {
+            res.status(412).json({ type: "no-such-product", message: "this item is not offered by this machine" });
+            return;
+        }
+
+        if (productRef.inventoryCount < qty) {
+            if (productRef.inventoryCount === 0) {
+                res.status(412).json({ type: "no-inventory", message: "item is out of stock" });
+                return;
+            }
+            
+            res.status(412).json({ type: "no-inventory", message: `Only ${productRef.inventoryCount} pcs are available` });
+            return;
+        }
+
+        const totalRaw = Object.entries(userAddedCoins)
+            .filter((e) => coinKeys.includes(e[0]))
+            .reduce((a, [field, count]) => (a += coinValueMap[field] * count), 0);
+        const totalGiven = Math.round(totalRaw * 100) / 100;
+        const totalNeeded = Math.round(qty * productRef.item.price * 100) / 100;
+
+        if (totalGiven < totalNeeded) {
+            res.status(412).json({ type: "no-enough-paid", message: "please add more coins" });
+            return;
+        }
+
+        const remaining = Math.round((totalGiven - totalNeeded) * 100) / 100;
+
+        machine.inventory = machine.inventory.map((invItem) =>
+            invItem.item._id.toString() === productId ? { ...invItem, inventoryCount: invItem.inventoryCount - qty } : invItem
+        );
+
+        if (remaining === 0) {
+            let overflow = machine.overflow;
+
+            coinKeys.forEach((field) => {
+                if (userAddedCoins[field] === 0) {
+                    return;
+                }
+
+                machine[field] = machine[field] + userAddedCoins[field];
+                if (machine[field] > 50) {
+                    const rem = machine[field] - 50;
+                    machine[field] = 50;
+                    overflow = overflow + rem * coinValueMap[field];
+                }
+            });
+
+            overflow = Math.round(overflow * 100) / 100;
+            machine.overflow = overflow;
+
+            await machine.updateOne({
+                $set: {
+                    inventory: machine.inventory,
+                    overflow: machine.overflow,
+                    oneCCoin: machine.oneCCoin,
+                    twoCCoin: machine.twoCCoin,
+                    fiveCCoin: machine.fiveCCoin,
+                    tenCCoin: machine.tenCCoin,
+                    twentyCCoin: machine.twentyCCoin,
+                    fiftyCCoin: machine.fiftyCCoin,
+                    oneDCoin: machine.oneDCoin,
+                    twoDCoin: machine.twoDCoin,
+                },
+            });
+
+            const updated = await MachineService.getOne(machineId);
+            res.status(200).json(updated);
+            return;
+        }
+
+        let calculationRemaining = remaining;
+        const coinValues = coinKeys.map((field) => coinValueMap[field]);
+        const machineAvailableCoins = coinKeys.map((field) => machine[field]);
+        const neededCoins = new Array(coinValues.length).fill(0);
+
+        for (let i = 0; i < coinValues.length; i++) {
+            const currentCoinValue = coinValues[i];
+
+            while (calculationRemaining >= currentCoinValue) {
+                neededCoins[i] += 1;
+
+                if (neededCoins[i] > machineAvailableCoins[i]) {
+                    neededCoins[i] -= 1;
+
+                    break;
+                }
+
+                calculationRemaining = Math.round((calculationRemaining - currentCoinValue) * 100) / 100;
+            }
+        }
+
+        if (calculationRemaining > 0) {
+            res.status(200).json({
+                type: "no-enough-coins",
+                message: `sorry, the machine does not have enough coins to return  ${calculationRemaining} EUR in change. Returning all user coins`,
+            });
+            return;
+        }
+
+        let overflow = machine.overflow;
+        for (let i = 0; i < neededCoins.length; i++) {
+            if (neededCoins[i] === 0) {
+                continue;
+            }
+
+            const field = coinKeys[i];
+            machine[field] = machine[field] - neededCoins[i] + userAddedCoins[field];
+            if (machine[field] > 50) {
+                const rem = machine[field] - 50;
+                machine[field] = 50;
+                overflow = overflow + rem * coinValueMap[field];
+            }
+        }
+
+        overflow = Math.round(overflow * 100) / 100;
+        machine.overflow = overflow;
+
+        await machine.updateOne({
+            $set: {
+                inventory: machine.inventory,
+                overflow: machine.overflow,
+                oneCCoin: machine.oneCCoin,
+                twoCCoin: machine.twoCCoin,
+                fiveCCoin: machine.fiveCCoin,
+                tenCCoin: machine.tenCCoin,
+                twentyCCoin: machine.twentyCCoin,
+                fiftyCCoin: machine.fiftyCCoin,
+                oneDCoin: machine.oneDCoin,
+                twoDCoin: machine.twoDCoin,
+            },
+        });
+
+        const updated = await MachineService.getOne(machineId);
+
+        res.status(200).json(updated);
+    } catch (error) {
+        console.log(error);
+        res.status(500);
+    }
 };
 
 const getNotAssignedProducts = async (req, res) => {
@@ -187,12 +351,7 @@ const refillCoins = async (req, res) => {
     try {
         const coinLabels = Object.keys(req.body);
         // todo find better way to not inject foreign keys
-        const updateObj = Object.assign(
-            {},
-            ...["oneCCoin", "twoCCoin", "fiveCCoin", "tenCCoin", "twentyCCoin", "fiftyCCoin", "oneDCoin", "twoDCoin"]
-                .filter((e) => coinLabels.includes(e))
-                .map((e) => ({ [e]: 50 }))
-        );
+        const updateObj = Object.assign({}, ...coinKeys.filter((e) => coinLabels.includes(e)).map((e) => ({ [e]: 50 })));
         const newInstance = await MachineService.updateOne(req.params.id, updateObj);
 
         res.status(200).json(newInstance.toObject());
@@ -320,12 +479,14 @@ router.put("/edit/:id", AuthMiddleware.isAuth, canTouch, edit);
 
 router.put("/coins/:id/refill", AuthMiddleware.isAuth, canTouch, refillCoins);
 
+router.put("/purchase/:machineId/:productId", purchase);
+
 router.put("/inventory/:id/add-products", AuthMiddleware.isAuth, canTouch, addProducts);
 router.put("/inventory/:id/edit-machine-products", AuthMiddleware.isAuth, canTouch, editMachineProducts);
 
 router.get("/inventory/:id/not-products", AuthMiddleware.isAuth, canTouch, getNotAssignedProducts);
 
-router.put("/inventory/:id/edit", AuthMiddleware.isAuth, canTouch, inventoryEdit);
+// router.put("/inventory/:id/edit", AuthMiddleware.isAuth, canTouch, inventoryEdit);
 
 // router.get("/vote/:id", isAuth, loadItem, canVote, vote);
 router.get("/delete/:id", AuthMiddleware.isAuth, canTouch, remove);
